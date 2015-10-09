2bb3a32a4f6e19cd4bd905f1dd10cef68aee05e9
silverweed <silverweed1991@gmail.com>
2015-10-09 17:27:32 +0200

    finish fuzzifier.opCall and create sample main

--- fam/fuzzifier.d
+++ fam/fuzzifier.d
@@ -1,20 +1,37 @@
 module fam.fuzzifier;
-import fam.types : FuzzyClass, Fitness, Fitnesses, RawInput;
 
+import fam.types;
+
+/// A component which converts raw input into fuzzified input.
+/// Raw input is an associative array of (double) values, while
+/// fuzzified input is an associative array of { class => fitness },
+/// where 'class' is defined by a trapezoidal shape and fitness
+/// is a double between 0 and 1.
+/// Once created, a Fuzzifier acts like a functor which can be called
+/// via the call operator, like:
+/// \code{.d}
+/// Fuzzifier f = new Fuzzifier(vars);
+/// auto fitnesses = f(data);
+/// \endcode
 class Fuzzifier {
 	this(FuzzyClass[][string] vars) {
 		this.vars = vars;
 	}
 
+	/// Converts raw input data (passed as a map
+	/// varname => value) into a Fitnesses result.
 	Fitnesses opCall(RawInput data) {
 		Fitnesses fitnesses;
 		foreach (varname, value; data) {
+			// Get possible classes for this variable
 			auto classes = vars[varname];
+			// Get fit for each class
 			foreach (fclass; classes) {
-				
+				auto fitness = fclass.fit(value);
+				fitnesses[varname ~ "." ~ fclass.name] = fitness;	
 			}
 		}
-		return null;
+		return fitnesses;
 	}
 
 	private const FuzzyClass[][string] vars;
--- fam/types.d
+++ fam/types.d
@@ -1,5 +1,9 @@
 module fam.types;
 
+alias Fitnesses = Fitness[string];
+alias RawInput = double[string];
+alias Fitness = double;
+
 /// FuzzyClass is a named range of values for a variable.
 /// It is defined by a name and 4 delimiter values, which
 /// define a trapezoid (which is the shape of the 'class function').
@@ -10,7 +14,7 @@ class FuzzyClass {
 		// TODO: sort delimiters
 	}
 
-	Fitness fit(double x) pure {
+	Fitness fit(double x) pure const {
 		if (x <= delimiters[0] || x >= delimiters[3]) 
 			return 0;
 		if (x >= delimiters[1] && x <= delimiters[2]) 
@@ -24,19 +28,3 @@ class FuzzyClass {
 	const string name;
 	const double[4] delimiters;
 }
-
-/// FuzzyVar represents a variable with a set of FuzzyClasses
-/// it can belong to. They implicitly define its range.
-/*class FuzzyVar {
-	this(string name, FuzzyClass[] classes) {
-		this.name = name;
-		this.classes = classes;
-	}
-
-	const string name;
-	const FuzzyClass[] classes;
-}*/
-
-alias Fitnesses = Fitness[string];
-alias RawInput = double[string];
-alias Fitness = double;

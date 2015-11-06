e4e077aba47cf63c232898afcf48817824761e48
bluehood <blue@lcm.mi.infn.it>
2015-11-06 12:58:23 +0100

    add steering script

---
+++ yafam.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+usage() {
+        echo "usage: $0 <def_file> <rule_file>"
+        exit 1
+}
+
+if (( $# < 2)); then
+        usage
+fi
+
+while (($# > 0)); do
+        case $1 in
+        "-b" | "--build") BUILDONLY=true; shift;;
+        \-*) usage;;
+        *) break;;
+        esac
+done
+
+DEFS="$1"
+RULES="$2"
+
+[[ ! -d bin ]] && mkdir bin
+make build &&
+./bin/parser "$DEFS" &&
+./parser/create_rules.sh "$RULES" &&
+make runtime
+
+if [[ $? -eq 0 ]]; then
+        [[ $BUILDONLY ]] && ./bin/yafam || exit 0
+else
+        echo "there was an error building yafam"
+        exit 2
+fi
